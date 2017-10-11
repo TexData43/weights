16ceda9fd45f62c66b7b91eb2810d06224e3be90
Tom <j.thomasmock@gmail.com>
2017-10-11 18:59:27 -0500

    Update weight_age.R

--- weight_age.R
+++ weight_age.R
@@ -18,20 +18,30 @@ wt_age <- wt_gather %>%
   filter(age_wt > 0)
 wt_age$weight <- as.numeric(wt_age$weight)
 
+### create a vector and then bin
+vec <- wt_age$age_wt
+bin_x <- seq(0, 1530, 30)
+vec2 <- .bincode(vec, bin_x, TRUE, TRUE)
+
+wt_age["bin"] <- vec2
+
 ### start the summarize pipe, exclude unknown genotypes, NA weights or outliers (> 55)
 ### group by SEX, GENOTYPE, and by the age in days then calc the mean
 wt_sum <- wt_age %>% 
-filter(GENOTYPE != "NA", GENOTYPE != "?", weight != "NA", weight < 55) %>% 
-  group_by(SEX, GENOTYPE, age_wt) %>% 
+  filter(GENOTYPE != "NA", GENOTYPE != "?", weight != "NA", weight < 55) %>% 
+  group_by(SEX, GENOTYPE, bin) %>% 
   summarise(n = n(), weight = mean(weight, na.rm = T))
 
 ### view the end results
 wt_sum
 
 ### Faceted graph by genotype and sex
-(g1 <- ggplot(data = wt_sum, aes(x = age_wt, y = weight, color = GENOTYPE)) + 
-    geom_line() + geom_point() + facet_wrap(~SEX, nrow = 2) + labs(x = "Age in Days", y = "Body weight in grams"))
-
+(g1 <- ggplot(data = wt_sum, aes(x = bin, y = weight, color = GENOTYPE)) + 
+    geom_point(size = 6, alpha = 0.2) + 
+    geom_smooth(alpha = .5, size = 1.5) + 
+    facet_wrap(~SEX, nrow = 2) + 
+    labs(x = "Age in months", y = "Body weight in grams") + 
+    scale_x_continuous(breaks = c(0, 5, 10, 15, 20, 25, 30)))
 ### save the plot
 ggsave("weight_age.png", width = 30, height = 20, units = "cm")
 
